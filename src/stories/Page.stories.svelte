2b7f574ad74b37c59d72219421eb91fca5d22891
Simon <simon>
2024-11-17 11:28:05 +0100

    init

---
+++ src/stories/Page.stories.svelte
@@ -0,0 +1,14 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Page from '../routes/+page.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Example/Page',
+    component: Page,
+    parameters: {
+      layout: 'fullscreen',
+    },
+  });
+</script>
+
+<Story name="Basic"  />
